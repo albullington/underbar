742d4ed2ce88cb4bc28d12e9248242d849fa729c
Amanda <albullington@gmail.com>
2018-02-21 18:17:12 -0800

    Everything except for uniq is completed in Amanda's Underbar.

--- src/underbar.js
+++ src/underbar.js
@@ -102,6 +102,13 @@
   _.uniq = function(array, isSorted, iterator) {
     var newArray = [];
 
+    return _.each(array, function(item) {
+      var checkIndex = _.indexOf(newArray, item);
+      if (checkIndex === -1) {
+        newArray.push(item);
+      }
+    });
+    return newArray;
   };
 
   // [1, 2, 3, 4]
@@ -164,17 +171,16 @@
   //   }); // should be 5, regardless of the iterator function passed in
   //          No accumulator is given so the first element is used.
   _.reduce = function(collection, iterator, accumulator) {
-     for (var i = 0; i < collection.length; i++) {
-      if (iterator === undefined) {
-          accumulator = collection[0];
-          collection = collection.slice(1);
-          accumulator = iterator(accumulator, collection[i]);
-      }  else {
-          accumulator = iterator(accumulator, collection[i]);
+    _.each(collection, function(item, index) {
+      if (accumulator === undefined && index === 0) {
+        accumulator = item;
+      } else {
+        accumulator = iterator(accumulator, item);
       }
-    }
+    });
     return accumulator;
   };
+
 
   // Determine if the array or object contains a given value (using `===`).
   _.contains = function(collection, target) {
@@ -192,20 +198,39 @@
 
   // Determine whether all of the elements match a truth test.
   _.every = function(collection, iterator) {
+    var isTrue = true;
+    iterator = iterator || _.identity;
+
+    if (collection.length === 0) {
+      return true;
+    }
     // TIP: Try re-using reduce() here.
     return _.reduce(collection, function(isTrue, item) {
-      if (isTrue) {
-        return true;
-      } else {
+      if (isTrue === false) {
         return false;
+      } else {
+        return Boolean(iterator(item));
       }
-    })
+    }, isTrue);
   };
 
   // Determine whether any of the elements pass a truth test. If no iterator is
   // provided, provide a default one
   _.some = function(collection, iterator) {
     // TIP: There's a very clever way to re-use every() here.
+    var isTrue = false;
+    iterator = iterator || _.identity;
+
+    if (collection.length === 0) {
+      return false;
+    }
+    return _.reduce(collection, function(isTrue, item) {
+      if (isTrue === true) {
+        return true;
+      } else {
+        return Boolean(iterator(item));
+      }
+    }, isTrue);
   };
 
 
@@ -228,11 +253,25 @@
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+    for(var i = 1; i < arguments.length; i++){
+      for (var key in arguments[i]){
+        obj[key] = arguments[i][key];
+      }
+    }
+    return obj 
   };
 
   // Like extend, but doesn't ever overwrite a key that already
   // exists in obj
   _.defaults = function(obj) {
+    for(var i = 1; i < arguments.length; i++){
+      for (var key in arguments[i]){
+        if (obj[key] === undefined) {
+          obj[key] = arguments[i][key];
+        }
+      }
+    }
+    return obj 
   };
 
 
@@ -276,6 +315,17 @@
   // already computed the result for the given argument and return that value
   // instead if possible.
   _.memoize = function(func) {
+    var cache = {};
+
+    return function(key) {
+      var args = [...arguments];
+      if (cache[key] !== undefined) {
+        return cache[key];
+      } else {
+        cache[args] = func.apply(this, args)
+        return cache[args];
+      }
+    } 
   };
 
   // Delays a function for the given number of milliseconds, and then calls
@@ -285,6 +335,10 @@
   // parameter. For example _.delay(someFunction, 500, 'a', 'b') will
   // call someFunction('a', 'b') after 500ms
   _.delay = function(func, wait) {
+      var args = [...arguments].slice(2);
+      return setTimeout(function() {
+        return func.apply(this, args);
+      }, wait);
   };
 
 
